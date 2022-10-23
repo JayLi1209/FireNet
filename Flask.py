ed827a214d41919d4e2f819f7f2a6c8f7a92ca3a
Daniel Little <daniel.t.little@vanderbilt.edu>
2022-10-22 21:59:45 -0500

    fire classes

--- Flask.py
+++ Flask.py
@@ -3,12 +3,19 @@ import pickle
 
 app = Flask(__name__)
 model = pickle.load(open('fire_prediction_model.sav', 'rb'))
-
+scaler = pickle.load(open('input_scaler_model.sav', 'rb'))
 
 @app.route("/", methods=['POST'])
 def predictor():
     data = request.get_json()
-    predictions = model.predict(data['x'])
-    return {"Risk": predictions.tolist(), "Location": "asdf", "data3": 1}
+    scaled_input = scaler.predict(data['x'])
+    prediction = model.predict(scaled_input)
+    if (prediction < 100):
+        size = 'small'
+    elif (prediction < 1000):
+        size = 'medium'
+    else:
+        size = 'large'
+    return {"Risk": size, "Location": "asdf", "data3": 1}
 
 
