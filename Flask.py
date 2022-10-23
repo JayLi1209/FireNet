50dd7ba93901d59827512c0a386d6b187da3368f
Junhao-Hui <junhao.hui@vanderbilt.edu>
2022-10-22 20:34:54 -0500

    r

--- Flask.py
+++ Flask.py
@@ -1,10 +1,9 @@
-from flask import Flask
 from flask import Flask, request
-import numpy as np
 import pickle
 
 app = Flask(__name__)
 model = pickle.load(open('fire_prediction_model.sav', 'rb'))
+
 
 @app.route("/", methods=['POST'])
 def predictor():
