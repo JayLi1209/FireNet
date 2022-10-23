7ad4138cacf374afbc7fe3071a6c1e8d6baeb208
Daniel Little <daniel.t.little@vanderbilt.edu>
2022-10-22 23:00:15 -0500

    model is complete

--- fire_prediction.py
+++ fire_prediction.py
@@ -3,39 +3,19 @@ Building the regression model from the dataset
 FW_Veg_Rem_Combined.csv is a dataset of US wildfires by location, weather, and other attributes
 """
 
-
 import pandas as pd
 from sklearn.linear_model import Ridge
-from sklearn.preprocessing import StandardScaler
 import pickle
-
-"""
-Normalizes the input data, which is in several columns of a dataframe
-"""
-def normalize_df(df):
-    result = df.copy()
-    ss = StandardScaler()
-    return pd.DataFrame(ss.fit_transform(result), columns=result.columns)
-
-"""
-Normalizes the single series that includes all output values
-"""
-def normalize_series(s):
-    values = s.values
-
-    values = values.reshape((len(values), 1))
-
-    scaler = StandardScaler()
-    scaler = scaler.fit(values)
-    return scaler.transform(values)
 
 # first gather the data we want to train our model on
 df = pd.read_csv('FW_Veg_Rem_Combined.csv')
+
+# remove rows that didn't record temperature
+df = df[df.Temp_pre_7 != -1.0]
+df = df[df.Temp_pre_7 != 0.0]
 feature_cols = ['latitude', 'longitude', 'Temp_pre_7', 'Wind_pre_7', 'Hum_pre_7']
-X = df.loc[:, feature_cols]
-y = df.loc[:, 'fire_size']
-X_train = normalize_df(X)
-y_train = normalize_series(y)
+X_train = df.loc[:, feature_cols]
+y_train = df.loc[:, 'fire_size']
 
 # creating the ridge regression model
 model = Ridge(alpha=3)
@@ -44,7 +24,7 @@ model = Ridge(alpha=3)
 model.fit(X_train, y_train)
 
 # Save the trained model as a .sav file
-filename = "fire_prediction_model.sav"
+filename = "fire_prediction_model_unscaled.sav"
 pickle.dump(model, open(filename, 'wb'))
 
 
