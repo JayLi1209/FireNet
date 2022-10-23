275595de99c2630d4c12f68879305aab16448458
Helen Sun <xutong.sun@vanderbilt.edu>
2022-10-22 19:55:29 -0500

    add counties

---
+++ public/al.js
@@ -0,0 +1,14 @@
+function populate(form)
+{
+form.options.length = 0;
+form.options[0] = new Option("Select a of county of Alabama","");
+form.options[1] = new Option("Jefferson","Jefferson");
+form.options[2] = new Option("Mobile","Mobile");
+form.options[3] = new Option("Madison","Madison");
+form.options[4] = new Option("Montgomery","Montgomery");
+form.options[5] = new Option("Baldwin","Baldwin");
+form.options[6] = new Option("Shelby","Shelby");
+form.options[7] = new Option("Tuscaloosa","Tuscaloosa");
+form.options[8] = new Option("Lee","Lee");
+form.options[9] = new Option("Morgan","Morgan");
+}
---
+++ public/dw.js
@@ -0,0 +1,9 @@
+function populate(form)
+{
+form.options.length = 0;
+form.options[0] = new Option("Select a of county of Delaware","");
+form.options[1] = new Option("New Castle","New Castle");
+form.options[2] = new Option("Sussex","Sussex");
+form.options[3] = new Option("Kent","Kent");
+
+}
