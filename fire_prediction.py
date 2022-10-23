02a1ac8709122e9488454e87ad84d7d2907533de
Junhao-Hui <junhao.hui@vanderbilt.edu>
2022-10-23 00:55:54 -0500

    90%

--- fire_prediction.py
+++ fire_prediction.py
@@ -5,20 +5,28 @@ FW_Veg_Rem_Combined.csv is a dataset of US wildfires by location, weather, and o
 
 import pandas as pd
 from sklearn.linear_model import Ridge
+from sklearn.model_selection import train_test_split
 import pickle
 
 # first gather the data we want to train our model on
 df = pd.read_csv('FW_Veg_Rem_Combined.csv')
 
-# remove rows that didn't record temperature
+# remove missing data
 df = df[df.Temp_pre_7 != -1.0]
 df = df[df.Temp_pre_7 != 0.0]
+df = df[df.Wind_pre_7 != -1.0]
+df = df[df.Wind_pre_7 != 0.0]
+df = df[df.Hum_pre_7 != -1.0]
+df = df[df.Hum_pre_7 != 0.0]
+
 feature_cols = ['latitude', 'longitude', 'Temp_pre_7', 'Wind_pre_7', 'Hum_pre_7']
-X_train = df.loc[:, feature_cols]
-y_train = df.loc[:, 'fire_size']
+X = df.loc[:, feature_cols]
+y = df.loc[:, 'fire_size']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=101)
 
 # creating the ridge regression model
-model = Ridge(alpha=3)
+model = Ridge(alpha=100)
 
 # fitting the model
 model.fit(X_train, y_train)
